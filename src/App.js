3cc06d38a2b78663dfee5f562cf6bd8e3c4358d0
SomayaAl <102401317+SomayaAl@users.noreply.github.com>
2022-06-23 18:47:10 -0400

    Created react-weather-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+      </div>
+      <footer>
+        This project was coded by Somaya Albadawi and is{" "}
+        <a href="">open-sourced.</a>
+      </footer>
     </div>
   );
 }
 
-export default App;
+
